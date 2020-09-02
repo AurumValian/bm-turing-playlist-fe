0a4510a626a6b698f70ead6f41b90636ef3c0ccd
Ben McClung <aurumvalian@gmail.com>
2020-09-02 11:15:19 -0600

    Build test for Songform

---
+++ src/components/SongForm/SongForm.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import SongForm from './SongForm';
+
+describe('SongForm', () => {
+  it('should display the inputs and buttons', () => {
+    render (
+      <SongForm />
+    )
+
+    const titleInput = screen.getByPlaceholderText('Title');
+    const artistInput = screen.getByPlaceholderText('Artist');
+    const linkInput = screen.getByPlaceholderText('Link');
+
+    expect(titleInput).toBeInTheDocument();
+    expect(artistInput).toBeInTheDocument();
+    expect(linkInput).toBeInTheDocument();
+  })
+})
